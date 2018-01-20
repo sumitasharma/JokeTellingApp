aa6ae085ce32e47af7dc0e2f4a0351d7381c3fa6
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-01-19 20:43:21 -0800

    feat: Added Android Connected Test
    I have added the android test.
    
    Detailed changes are as followed:
     - The test will see if the JokeTellingApp which has a button gets clicked.
     - Checks if the result fetches some data from Asynchronous task.

--- JokeTellingLibrary/src/main/java/com/example/sumitasharma/joketellinglibrary/MyJokes.java
+++ JokeTellingLibrary/src/main/java/com/example/sumitasharma/joketellinglibrary/MyJokes.java
@@ -1,9 +1,23 @@
 package com.example.sumitasharma.joketellinglibrary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MyJokes {
+    private static final ArrayList<String> jokesArrayList =
+            new ArrayList<String>(Arrays.asList("You know why you never see elephants hiding up in trees... Because they're really good at it.",
+                    "What's the difference between a guitar and a fish. You can't tuna fish.",
+                    "What is red and smells like blue paint?. Red paint",
+                    "What do you get from a pampered cow? Spoiled milk.",
+                    "What do you call bears with no ears? B.",
+                    "Did you hear about the hungry clock? It went back four seconds.",
+                    "Why dont blind people skydive? Because it scares the crap out of their dogs.",
+                    "What do you call a baby monkey? A Chimp off the old block."));
 
     public String getJokes() {
-        return "My Joke. What a joke.";
+        int upperLimit = jokesArrayList.size();
+        int jokeIndex = (int) Math.floor(Math.random() * upperLimit);
+        return jokesArrayList.get(jokeIndex);
     }
 
 }
---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,28 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void onClickButtonTest() {
+        onView(withId(R.id.tell_joke_button))
+                .perform(click());
+        onView(withId(R.id.joke_telling_text)).check(matches(isDisplayed()));
+    }
+}
